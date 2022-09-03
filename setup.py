0e14fcba48572d6f0524b122e69775f6cca50384
Hao Sun <nagisa940216@gmail.com>
2022-09-03 15:04:27 +0800

    fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (base_dir / 'README.md').read_text()
 
 setup(
     name='nebula-model',
-    version='0.0.2',
+    version='0.1.0',
     author='Sword Elucidator',
     author_email='nagisa940216@gmail.com',
     url='https://github.com/SwordElucidator/nebula-model',
